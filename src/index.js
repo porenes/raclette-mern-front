9ef77b8fd0972aa444de3df89feb14ec54eb49e1
patxol <pol@lemonbox.fr>
2021-01-31 22:42:35 +0100

    ➕ 💄 Adding npm bootstrap and custom theme

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import "./styles.scss";
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 import React from "react";
 import ReactDOM from "react-dom";
 // Redux imports
