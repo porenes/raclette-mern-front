52dcc188fd22a1f7b5894a90b2c7615d7fbb9db9
patxol <pol@lemonbox.fr>
2021-02-06 16:23:38 +0100

    🩹 Do not diaplay woo buttons if not logged in

--- src/components/Connoisseur/Woo.js
+++ src/components/Connoisseur/Woo.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { useDispatch, useSelector } from "react-redux";
-import { isEmpty } from "../../Utils";
 import { unwoo, woo } from "../../actions/user.actions";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { isEmpty } from "../../Utils";
 
 const Woo = ({ author }) => {
   const UNWOO = (
@@ -42,58 +42,60 @@ const Woo = ({ author }) => {
   };
 
   useEffect(() => {
-    console.log(userData.wooed);
-    console.log(author._id);
+    console.log(!isEmpty(userData));
     userData.wooeds && userData.wooeds.includes(author._id)
       ? setIsWooed(true)
       : setIsWooed(false);
-    
+
     userData.wooers && userData.wooers.includes(author._id)
       ? setIsWooer(true)
       : setIsWooer(false);
-    
   }, [userData, author]);
 
-  return isWooed ? (
-    isWooer ?
-
-    <button
-      type="button"
-      className="btn btn-success float-right"
-      onClick={handleUnWoo}
-      onMouseEnter={() => setCompeerText(UNWOO)}
-      onMouseLeave={() => setCompeerText(COMPEER)}
-    >
-      {compeerText}
-    </button>
-    :
-    <button
-      type="button"
-      className="btn btn-primary float-right"
-      onClick={handleUnWoo}
-      onMouseEnter={() => setWooedText(UNWOO)}
-      onMouseLeave={() => setWooedText(WOOED)}
-    >
-      {wooedText}
-    </button>
-  ) : (
-    <button
-      type="button"
-      className={notWooedOver?"btn btn-primary float-right":"btn btn-secondary float-right"}
-      onClick={handleWoo}
-      onMouseEnter={() => setNotWooedOver(true)}
-      onMouseLeave={() => setNotWooedOver(false)}
-    >
-      {notWooedOver?
-      <>
-        <FontAwesomeIcon icon="heart" /> Woo !
-      </>
-      :
-      <>
-         Woo ?
-      </>}
-    </button>
-  );
+  if (!isEmpty(userData))
+    {return isWooed ? (
+      isWooer ? (
+        <button
+          type="button"
+          className="btn btn-success float-right"
+          onClick={handleUnWoo}
+          onMouseEnter={() => setCompeerText(UNWOO)}
+          onMouseLeave={() => setCompeerText(COMPEER)}
+        >
+          {compeerText}
+        </button>
+      ) : (
+        <button
+          type="button"
+          className="btn btn-primary float-right"
+          onClick={handleUnWoo}
+          onMouseEnter={() => setWooedText(UNWOO)}
+          onMouseLeave={() => setWooedText(WOOED)}
+        >
+          {wooedText}
+        </button>
+      )
+    ) : (
+      <button
+        type="button"
+        className={
+          notWooedOver
+            ? "btn btn-primary float-right"
+            : "btn btn-secondary float-right"
+        }
+        onClick={handleWoo}
+        onMouseEnter={() => setNotWooedOver(true)}
+        onMouseLeave={() => setNotWooedOver(false)}
+      >
+        {notWooedOver ? (
+          <>
+            <FontAwesomeIcon icon="heart" /> Woo !
+          </>
+        ) : (
+          <>Woo ?</>
+        )}
+      </button>
+    );}else return <></>
 };
 
 Woo.propTypes = {
