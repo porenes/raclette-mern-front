a3c6ea35b8c2a82dc60a3c4f897a71e3a35ac955
patxol <pol@lemonbox.fr>
2021-02-13 16:34:41 +0100

    💄 product display improvement

--- src/components/Products/ProductsList.js
+++ src/components/Products/ProductsList.js
@@ -1,3 +1,4 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useState } from "react";
 import { Card, Container } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
@@ -16,18 +17,26 @@ const ProductsList = () => {
     }
   }, [loadProducts, dispatch]);
   return (
-    <Container>
+    <Container className="bg-light rounded p-2">
+      <h5>Donnez votre avis !</h5>
       {!isEmpty(productsState[0]) &&
         productsState.map((product) => {
           return (
-            <Card>
-              <Card.Img src={product.image_small_url} variant="top" />
+            <Card className="mb-3 bg-dark text-white">
+              <Card.Img
+                src={product.image_small_url}
+                style={{ filter: "brightness(50%)" }}
+              />
+              <Card.ImgOverlay>
+                <Card.Text>n reviews</Card.Text>
+              </Card.ImgOverlay>
               <Card.Header>
-                <Card.Title>
-                  {product.product_name + " - " + product.brands}
-                </Card.Title>
+                <Card.Title>{product.product_name}</Card.Title>
+                <Card.Subtitle>
+                  <FontAwesomeIcon icon="box" />
+                  {" " + product.brands}
+                </Card.Subtitle>
               </Card.Header>
-              <Card.Body></Card.Body>
             </Card>
           );
         })}
