1fb265fa6cd7988e9a421a69b42be54fe3e02afd
patxol <pol@lemonbox.fr>
2021-02-23 21:10:04 +0100

    🎨 remove unused dep

--- src/components/Products/ProductsList.js
+++ src/components/Products/ProductsList.js
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useState } from "react";
-import { Card, Container, Media, Spinner } from "react-bootstrap";
+import { Container, Media, Spinner } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { listProducts } from "../../actions/products.action";
 import { isEmpty } from "../../Utils";
@@ -28,7 +28,13 @@ const ProductsList = () => {
           return (
             <>
               <Media key={product._id} className="p-3">
-                <img src={product.image_small_url} alt={product._id} height="1%" width="20%"className="align-self-center mr-3"/>
+                <img
+                  src={product.image_small_url}
+                  alt={product._id}
+                  height="1%"
+                  width="20%"
+                  className="align-self-center mr-3"
+                />
                 <Media.Body>
                   <h6>
                     {product.product_name} - <FontAwesomeIcon icon="box" />
@@ -36,7 +42,7 @@ const ProductsList = () => {
                   </h6>
                 </Media.Body>
               </Media>
-              <hr/>
+              <hr />
             </>
           );
         })}
